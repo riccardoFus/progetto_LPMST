51a9641ae45b434c3b43295a099018a0757874da
Cesare <cesare.lever@studenti.unitn.it>
2024-04-24 14:41:49 +0200

    Aggiunti check login e signin

--- shelfy/app/src/main/java/com/example/shelfy/ui/screens/SignIn.kt
+++ shelfy/app/src/main/java/com/example/shelfy/ui/screens/SignIn.kt
@@ -227,7 +227,7 @@ fun SignIn(
                             unfocusedPlaceholderColor = BlueText,
                             focusedPlaceholderColor = BlueText
                         ),
-                        visualTransformation = if(passwordPressed) PasswordVisualTransformation() else VisualTransformation.None
+                        visualTransformation = if(!passwordPressed) PasswordVisualTransformation() else VisualTransformation.None
                     )
                 }
                 var password2 by rememberSaveable { mutableStateOf("") }
@@ -269,7 +269,7 @@ fun SignIn(
                             unfocusedPlaceholderColor = BlueText,
                             focusedPlaceholderColor = BlueText
                         ),
-                        visualTransformation = if(password2Pressed) PasswordVisualTransformation() else VisualTransformation.None
+                        visualTransformation = if(!password2Pressed) PasswordVisualTransformation() else VisualTransformation.None
                     )
                 }
 
